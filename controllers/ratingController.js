3ad0c7b418b51681deb3fba8e077e37aeff9f54a
samitshrestha29 <samitshrestha29@gmail.com>
2024-01-16 12:07:33 +0545

    check user rating controller

---
+++ controllers/ratingController.js
@@ -0,0 +1,103 @@
+const Food = require("../models/Food");
+const Rating = require("../models/Rating");
+const Restaurant = require("../models/Restaurant");
+
+module.exports = {
+  addRating: async (req, res) => {
+    const newRating = new Rating({
+      userId: req.user.id,
+      ratingType: req.body.rating,
+      product: req.body.product,
+      rating: req.body.rating,
+    });
+
+    try {
+      await newRating.save();
+      if (req.body.ratingType === "Restaurant") {
+        const restaurant = await Rating.aggregate([
+          {
+            $match: {
+              ratingType: req.body.ratingType,
+              product: req.body.product,
+            },
+          },
+          {
+            $group: {
+              _id: "$product",
+              averageRating: { $avg: "$rating" },
+            },
+          },
+        ]);
+
+        if (restaurant.length > 0) {
+          const averageRating = restaurant[0].averageRating; // Fix typo here
+          await Restaurant.findByIdAndUpdate(
+            req.body.product,
+            {
+              rating: averageRating,
+            },
+            { new: true }
+          );
+        }
+      } else if (req.body.ratingType === "Food") {
+        const restaurant = await Rating.aggregate([
+          {
+            $match: {
+              ratingType: req.body.ratingType,
+              product: req.body.product,
+            },
+          },
+          {
+            $group: {
+              _id: "$product",
+              averageRating: { $avg: "$rating" },
+            },
+          },
+        ]);
+
+        if (restaurant.length > 0) {
+          const averageRating = restaurant[0].averageRating;
+          await Restaurant.findByIdAndUpdate(
+            req.body.product,
+            {
+              rating: averageRating,
+            },
+            { new: true }
+          );
+        }
+      }
+      res
+        .status(200)
+        .json({ status: true, message: "Rating updated successfully" });
+    } catch (error) {
+      res.status(500).json({ status: false, message: error.message });
+    }
+  },
+
+  checkUserRating: async (req, res) => {
+    const ratingType = req.query.ratingType;
+    const product = req.query.product;
+
+    try {
+      const existingRating = await Rating.findOne({
+        userId: req.user.id,
+        product: product,
+        ratingType: ratingType,
+      });
+
+      if (existingRating) {
+        res.status(200).json({
+          status: true,
+          message: "User have already rated this restaurant ",
+        });
+      } else {
+        res.status(200).json({
+          status: false,
+          message: "USer has not rated this restaurant",
+        });
+      }
+    } catch (error) {
+      res.status(500).json({ status: false, message: error.message });
+    }
+  },
+};
